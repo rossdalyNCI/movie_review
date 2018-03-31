e8f866da5ea1fef2a9097b664493eeb89eeead40
EC2 Default User <ec2-user@ip-172-31-21-120.us-east-2.compute.internal>
2018-03-31 16:40:57 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  
+  resources :movies do
+    resources :reviews, except: [:show, :index]
+  end
+  
+  root 'movies#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
